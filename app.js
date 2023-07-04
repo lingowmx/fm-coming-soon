2727b49be1b8c6b27cfc1f68fe9fb8a316d2f5a1
lingowmx <clingow@hotmail.com>
2023-07-04 18:22:15 -0500

    second commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('.container__content__form');
+const inputEmail = document.querySelector('.container__content__form__email');
+const error = document.querySelector('.container__content__form__email--error');
+
+form.addEventListener('submit', (e) => {
+   
+    const rexExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+
+    if(rexExp.test(inputEmail.value) === true){
+        inputEmail.style.border = '1px solid hsl(0, 36%, 70%)';
+        inputEmail.style.backgroundImage = 'url("")';
+        error.innerHTML = '';
+    }else{
+        e.preventDefault(); 
+        inputEmail.style.border = '1px solid red';
+        inputEmail.style.backgroundImage = 'url("./images/icon-error.svg")';
+        error.innerHTML = 'Please provide a valid email';
+    }
+})
